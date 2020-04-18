b5f2ebd9d6617071a548550f85352337c8901e47
Yidan Ma <madt@bc.edu>
2020-04-17 20:34:47 -0400

    7.19 -  Getting the Device Location with CoreLocation

--- WeatherGift/LocationDetailViewController.swift
+++ WeatherGift/LocationDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 private let dateFormatter: DateFormatter = {
     let dateFormatter = DateFormatter()
@@ -26,6 +27,7 @@ class LocationDetailViewController: UIViewController {
     
     var locationIndex = 0
     var weatherDetail: WeatherDetail!
+    var locationManager: CLLocationManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +43,9 @@ class LocationDetailViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
         
+        if locationIndex == 0 {
+            getLocation()
+        }
         
         updateUserInterface()
     }
@@ -133,3 +138,65 @@ extension LocationDetailViewController: UICollectionViewDataSource, UICollection
     }
     
 }
+
+extension LocationDetailViewController: CLLocationManagerDelegate {
+    func getLocation() {
+        // Creating a CLLocationManager will automatically check authorization
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("Checking authentication status")
+        handleAuthenticalStatus(status: status)
+    }
+    
+    func handleAuthenticalStatus(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            self.oneButtonAlert(title: "Location services denied", message: "It may be that parental controls are restricting location use in this app.")
+        case .denied:
+            //TODO
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        @unknown default:
+            print("Developer Alert: unknown case of status in handleAuthenticalStatus\(status)")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // TODO
+        let currentLocation = locations.last ?? CLLocation()
+        print("Current location is \(currentLocation.coordinate.latitude)\(currentLocation.coordinate.longitude)")
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(currentLocation) { (placemarks, error) in
+            var locationName = ""
+            if placemarks != nil {
+                // get the first placemark
+                let placemark = placemarks?.last
+                // assign placemark to locationName
+                locationName = placemark?.name ?? "Parts Unknown"
+            } else {
+                print("ERROR: retrieving place.")
+                locationName = "Could not find location"
+            }
+            print("🗺 locationName = \(locationName)")
+            
+            // Update weatherLocation[0] with the current location so it can be used in updateUserInterface. getLocation only called when locationIndex == 0
+            let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+            pageViewController.weatherLocations[self.locationIndex].latitude = currentLocation.coordinate.latitude
+            pageViewController.weatherLocations[self.locationIndex].longitude = currentLocation.coordinate.longitude
+            pageViewController.weatherLocations[self.locationIndex].name = locationName
+            self.updateUserInterface()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("ERROR: \(error.localizedDescription). Failed to get device location.")
+    }
+}
+
+
